91af594609c93e1bdc3c31acb23e9845d196e13e
lijijeling <qoo40328@gmail.com>
2024-06-15 23:16:26 +0800

    first commit

---
+++ FoodChallenge-main/Assets/C#/MedalManager.cs
@@ -0,0 +1,7 @@
+public static class MedalManager
+{
+    public static bool GamePlayed { get; set; } = false;
+    public static float TotalTime { get; set; } = 0;
+    public static bool GamePass { get; set; } = false;
+
+}
